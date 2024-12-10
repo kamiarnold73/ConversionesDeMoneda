3015cbeedb7c2df5e2babcbde6335764e94d8620
kamiarnold73 <kamilla_arnold24@unikino.edu.mx>
2024-12-09 21:59:07 -0700

    Quedó listo

--- ConversionesDeMoneda/Program.cs
+++ ConversionesDeMoneda/Program.cs
@@ -18,7 +18,13 @@ namespace ConversionesDeMoneda
                         ConvertirPesosADolares();
                         break;
                     case "2":
-                        Console.WriteLine("Dolares a pesos");
+                        ConvertirDolaresAPesos();
+                        break;
+                    case "3":
+                        ConvertirPesosAEuros();
+                        break;
+                    case "4":
+                        ConvertirEurosAPesos();
                         break;
                     default:
                         Console.WriteLine("Opcion incorrecta");
@@ -36,7 +42,8 @@ namespace ConversionesDeMoneda
                 Console.WriteLine("Conversiones");
                 Console.WriteLine("1) Pesos a dolares");
                 Console.WriteLine("2) Dolares a pesos");
-                Console.WriteLine("");
+                Console.WriteLine("3) Pesos a euros");
+                Console.WriteLine("4) Euros a pesos");
             }
             static void ConvertirPesosADolares() 
             {
@@ -60,5 +67,74 @@ namespace ConversionesDeMoneda
                 dolares = pesos / tipoCambio;
                 return dolares;
             }
+
+            static void ConvertirDolaresAPesos()
+            {
+            Console.WriteLine("dolares a pesos");
+            Console.WriteLine("Introducir tipo de cambio");
+
+            string respuestaUsuario = Console.ReadLine();
+            decimal.TryParse(respuestaUsuario, out decimal tipoDeCambio);
+
+
+            Console.WriteLine("Introducir la cantidad de dolares");
+            respuestaUsuario = Console.ReadLine();
+            decimal.TryParse(respuestaUsuario, out decimal dolares);
+
+            decimal pesos = DolaresAPesos(tipoDeCambio, dolares);
+            Console.WriteLine($"Cantidad en pesos: {pesos}");
         }
+            static decimal DolaresAPesos(decimal tipoCambio, decimal dolares)
+            {
+               decimal pesos = 0.00m;
+               pesos = dolares / tipoCambio;
+               return pesos;
+            }
+
+        static void ConvertirPesosAEuros()
+        {
+            Console.WriteLine("pesos a euros");
+            Console.WriteLine("Introducir tipo de cambio");
+
+            string respuestaUsuario = Console.ReadLine();
+            decimal.TryParse(respuestaUsuario, out decimal tipoDeCambio);
+
+
+            Console.WriteLine("Introducir la cantidad de pesos");
+            respuestaUsuario = Console.ReadLine();
+            decimal.TryParse(respuestaUsuario, out decimal pesos);
+
+            decimal euros = PesosAEuros(tipoDeCambio, pesos);
+            Console.WriteLine($"Cantidad en euros: {euros}");
+        }
+        static decimal PesosAEuros(decimal tipoCambio, decimal pesos)
+        {
+            decimal euros = 0.00m;
+            euros = pesos / tipoCambio;
+            return euros;
+        }
+
+        static void ConvertirEurosAPesos()
+        {
+            Console.WriteLine("euros a pesos");
+            Console.WriteLine("Introducir tipo de cambio");
+
+            string respuestaUsuario = Console.ReadLine();
+            decimal.TryParse(respuestaUsuario, out decimal tipoDeCambio);
+
+
+            Console.WriteLine("Introducir la cantidad de euros");
+            respuestaUsuario = Console.ReadLine();
+            decimal.TryParse(respuestaUsuario, out decimal euros);
+
+            decimal pesos = EurosAPesos(tipoDeCambio, euros);
+            Console.WriteLine($"Cantidad en pesos: {pesos}");
+        }
+        static decimal EurosAPesos(decimal tipoCambio, decimal euros)
+        {
+            decimal pesos = 0.00m;
+            pesos = euros / tipoCambio;
+            return pesos;
+        }
+    }
     }
